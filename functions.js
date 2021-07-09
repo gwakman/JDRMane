013c8a5981cca51b4a5d37994f0cea94b6de360c
Glitch (jdrmane) <none>
2021-07-09 12:58:31 +0000

    ⛰🚓 Updated with Glitch

--- functions.js
+++ functions.js
@@ -1,12 +1,26 @@
-function hide(a){
-  pchild=a.parentNode.getElementsByTagName('div');
-  if(pchild[0].style.visibility!='hidden'){
-    pchild[0].style.visibility='hidden';
-    pchild[0].style.height='0';
-    pchild[0].style.marginTop='';
+function hide(a) {
+  var divchild = a.parentNode.getElementsByTagName("div");
+  if (divchild[0].style.visibility != "hidden") {
+    divchild[0].style.visibility = "hidden";
+    divchild[0].style.height = "0";
+    divchild[0].style.marginTop = "";
   } else {
-    pchild[0].style.visibility='';
-    pchild[0].style.height='';
-    pchild[0].style.marginTop='10px';
+    divchild[0].style.visibility = "";
+    divchild[0].style.height = "";
+    divchild[0].style.marginTop = "10px";
   }
+}
+
+function slide(classname, item) {
+  var divcontent = document.getElementsByClassName("content");
+  for (var i = 0; i < divcontent.length; i++) {
+    divcontent[i].style.visibility = "hidden";
+  }
+  var div = document.getElementsByClassName(classname);
+  div[0].style.visibility = "";
+  var tabitem = document.getElementsByClassName('tab-item')
+  for (var i = 0; i < tabitem.length; i++) {
+    tabitem[i].classList.remove('active');
+  }
+  item.classList.add('active')
 }
